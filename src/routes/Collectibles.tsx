3f625a1ba91e3009dba33d4226e907f8e53a5394
Sergey Zhigunov <i.sergey.zhigunov@gmail.com>
2019-09-22 15:18:22 +0300

    add collectibles

--- src/routes/Collectibles.tsx
+++ src/routes/Collectibles.tsx
@@ -1,36 +1,81 @@
-import React from 'react';
+import React from "react";
 
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
-import Button from '@material-ui/core/Button';
-import Box from '@material-ui/core/Box';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Box from "@material-ui/core/Box";
+import Avatar from '@material-ui/core/Avatar';
+import Badge from '@material-ui/core/Badge';
+import ImageIcon from '@material-ui/icons/VerifiedUser';
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-      flexGrow: 1,
-    },
-    introBox: {
-      
-    }
+  root: {
+    width: "100%",
+    maxWidth: 360
+  },
+  collectiblesBox: {}
 }));
 
-const Collectibles:React.SFC = () => {
-    const classes = useStyles();
+const Collectibles: React.SFC = () => {
+  const classes = useStyles();
 
-    return (
-        <Box className={classes.introBox}>
-            <Paper className={classes.root}>
-                <Typography variant="h5" component="h3">
-                    This is a sheet of paper.
-                </Typography>
-                <Typography component="p">
-                    Paper can be used to build surface or other elements for your application.
-                </Typography>
-            </Paper>
-        </Box>
-    )
-}
+  return (
+    <Box className={classes.collectiblesBox}>
+      <Box
+        style={{
+          display: "flex",
+          alignContent: "center",
+          flexDirection: "row"
+        }}
+      >
+        <Typography variant="h5" component="p">
+          John <b>DOE</b> Collectibles
+        </Typography>
+      </Box>
+      <Divider />
+      <List className={classes.root}>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              JJ
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Joanna JOSE" secondary="Jan 9, 2014 | kyiv, UA" />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              JD
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="John DOE" secondary="Jan 7, 2014" />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Avatar>
+              DJ
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary="Dow JONES" secondary="July 20, 2014 | new york, USA" />
+        </ListItem>
+        <ListItem>
+          <ListItemAvatar>
+            <Badge overlap="circle" anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }} badgeContent={<ImageIcon color="action"/>}>
+                <Avatar>
+                     PJ
+                </Avatar>
+            </Badge>
+          </ListItemAvatar>
+          <ListItemText primary="Polly Jean HARVEY" secondary="July 21, 2014 | glastonbury, UK" />
+        </ListItem>
+      </List>
+    </Box>
+  );
+};
 
 export default Collectibles;
