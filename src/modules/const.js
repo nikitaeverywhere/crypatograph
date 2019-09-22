c47446509e1eb5f9aa08e10d71e19ceb4b23ab4d
Nikita Savchenko <zitros.lab@gmail.com>
2019-09-22 15:39:04 +0300

    Go prod

--- src/modules/const.js
+++ src/modules/const.js
@@ -1,3 +1,3 @@
 export const contractAddress = '0xf3214fa89da13b1138be07c9a020e28f9db2a627';
 // export const delegatedBackEndUrl = 'https://dapp.ml/delegated-tx';
-export const delegatedBackEndUrl = 'http://localhost:8088';
+export const delegatedBackEndUrl = 'https://dapp.ml/delegated-tx';
