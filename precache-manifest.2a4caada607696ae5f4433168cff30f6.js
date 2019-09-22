334dbea4ee6c28099b6a10404f8303aca0a1763a
Nikita Savchenko <zitros.lab@gmail.com>
2019-09-22 21:26:21 +0300

    Updates

---
+++ precache-manifest.2a4caada607696ae5f4433168cff30f6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f105107c76a0eecda43e97afcd2f28",
+    "url": "/crypatograph/index.html"
+  },
+  {
+    "revision": "13900ec1aa70037b9df6",
+    "url": "/crypatograph/static/css/main.e04360f9.chunk.css"
+  },
+  {
+    "revision": "c7fca682a6f92e00dc98",
+    "url": "/crypatograph/static/js/2.2a151de1.chunk.js"
+  },
+  {
+    "revision": "13900ec1aa70037b9df6",
+    "url": "/crypatograph/static/js/main.04d5166b.chunk.js"
+  },
+  {
+    "revision": "e3bd264f18e7a47c2674",
+    "url": "/crypatograph/static/js/runtime-main.ffee82da.js"
+  }
+]);
